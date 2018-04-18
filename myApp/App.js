95fcb95a6ecc204bb432ff874c8564eeaf326e5b
Cristian Carcaño <38365802+moptix10@users.noreply.github.com>
2018-04-18 20:29:04 -0300

    11
    11

--- myApp/App.js
+++ myApp/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>109</Text>
+        <Text>1011</Text>
         <Text>another add1</Text>
         <Text>another add2</Text>
       </View>
